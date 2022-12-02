270d7597bbbc76a9b4cef20a31d172af3d36078c
unknown <abhimvj575@gmail.com>
2022-12-02 20:28:29 +0530

    [Abhilash] Part time is calculated

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -5,16 +5,19 @@
         static void Main(string[] args)
         {
             int FullTime = 1;
+            int PartTime = 2;
             int EmpRatePerHr = 20;
             int EmpHrs = 0;
             int EmpWage = 0;
             Random randobj=new Random();
-            int checkpresent = randobj.Next(0, 2);
-            if (checkpresent == FullTime)
+            int checkpresent = randobj.Next(0, 3);
+            if (checkpresent == PartTime)
+            {
+                EmpHrs = 4;
+            }
+            else if (checkpresent == FullTime) 
             {
                 EmpHrs = 8;
-
-            
             }
             else
             {
