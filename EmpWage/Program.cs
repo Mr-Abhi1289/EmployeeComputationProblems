da9de9624546b4108a37b270e032e3c67044b261
unknown <abhimvj575@gmail.com>
2022-12-02 21:14:40 +0530

    [Abhilash ] Calculating Wages for a month assume 20 days

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -8,31 +8,44 @@ namespace EmpWage
         public const int FullTime = 1;
         public const int PartTime = 2;
         public const int EmpRatePerHr = 20;
+        public const int NumberofWorkingDays = 20;
 
 
         static void Main(string[] args)
         {
-        int EmpHrs = 0;
+            int EmpHrs = 0;
             int EmpWage = 0;
-            Random random=new Random();
-            int empcheck = random.Next(0, 3);
-            switch (empcheck)
+            int TotalEmpWage = 0;
+
+
+
+            for ( int day = 0; day < NumberofWorkingDays; day++)
             {
-                case PartTime:
-                    EmpHrs = 4;
-                    break;
-                case FullTime:
-                    EmpHrs = 8;
-                    break;
-                default:
-                    EmpHrs = 0;
-                    break;
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
+     
+                 switch (empcheck)
+                {
+                    case PartTime:
+                        EmpHrs = 4;
+                        break;
+                    case FullTime:
+                        EmpHrs = 8;
+                        break;
+                    default:
+                        EmpHrs = 0;
+                        break;
+                }
 
 
+
+                EmpWage = EmpHrs * EmpRatePerHr;
+                TotalEmpWage = TotalEmpWage + EmpWage;
+                Console.WriteLine("Employee daily wage:" + EmpWage);
             }
-           
-            EmpWage = EmpHrs * EmpRatePerHr;
-            Console.WriteLine("Employee daily wage:" + EmpWage);
+            Console.WriteLine("total employee wage:"+TotalEmpWage);
+
         }
+           
     }
 }
